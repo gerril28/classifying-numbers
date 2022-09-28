b617a5e4e09a5885e885937f178cc990fe8ad260
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-28 09:07:04 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
 input.onButtonPressed(Button.A, function () {
     number += 1
+    basic.showNumber(number)
 })
 input.onButtonPressed(Button.AB, function () {
     if (number > 0) {
         basic.showString("N")
     } else if (number < 0) {
         basic.showString("P")
+    } else if (0 == 0) {
+        basic.showString("Z")
     } else {
     	
     }
 })
 input.onButtonPressed(Button.B, function () {
     number += -1
+    basic.showNumber(number)
 })
 // By the programmer
 // 
