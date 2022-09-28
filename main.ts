0a7eeef1d8e23802fb41b9a9cb1954537c009e59
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-28 09:00:57 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    number += 1
 })
+input.onButtonPressed(Button.AB, function () {
+    if (number > 0) {
+        basic.showString("N")
+    } else if (0 < 0) {
+    	
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    number += -1
+})
+// By the programmer
+// 
+let number = 0
+number = 7
